03bf14c70329be1246ca63a2f1ec35bc5ae58199
Dũng <dotiendung1234567891@gmail.com>
2024-05-09 22:21:55 +0700

    done

--- src/sched.c
+++ src/sched.c
@@ -59,7 +59,6 @@ struct pcb_t *get_mlq_proc(void)
 	 * Remember to use lock to protect the queue.
 	 */
 	int flag = 0;
-	int index = 0;
 	for (int i = 0; i < MAX_PRIO; ++i)
 	{
 		if (empty(&mlq_ready_queue[i]))
